7c918a38b59499a215721fa16e754fcde36f84c4
godzzbboss <1027785756@qq.com>
2022-06-19 11:16:54 +0800

    第一章学习结束0618

--- chapter1-开始/code/test.cpp
+++ chapter1-开始/code/test.cpp
@@ -14,15 +14,19 @@ using namespace std;
 int main()
 {
     int sum = 0, value = 0;
+    
     // 将标准输入流定位到input.txt
     freopen("./input.txt", "r", stdin);
+
     while (cin >> value)
     {
         sum += value;
     }
+
     // 将标准输出流定位到output.txt
     freopen("./output.txt", "w", stdout);
     cout << "Sum is:" << sum << endl;
+
     return 0;
 }
 
