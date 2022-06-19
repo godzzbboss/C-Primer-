340947f545e9e7d7c1f31e009fa521142adcc9b4
godzzbboss <1027785756@qq.com>
2022-06-19 13:58:03 +0800

    第二章

---
+++ chapter2-变量和基本类型/code/基本数据类型.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool flag = 43;
+    cout << flag << endl;
+    cout << true << endl;
+    cout << false << endl;
+    cout << (flag == true) << endl;
+
+    return 0;
+} 
