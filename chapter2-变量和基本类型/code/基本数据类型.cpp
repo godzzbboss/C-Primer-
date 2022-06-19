3a5a1e9abd43ba4ceb49c384fab5e8eaea024394
godzzbboss <1027785756@qq.com>
2022-06-20 00:27:42 +0800

    C++基本数据类型

--- chapter2-变量和基本类型/code/基本数据类型.cpp
+++ chapter2-变量和基本类型/code/基本数据类型.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
+// int main()
+// {
+//     bool flag = 43; // flag = 1,bool只能取0或1
+//     cout << flag << endl;
+//     cout << true << endl;
+//     cout << false << endl;
+//     cout << (flag == true) << endl;
+
+//     return 0;
+// } 
+
 int main()
 {
-    bool flag = 43;
-    cout << flag << endl;
-    cout << true << endl;
-    cout << false << endl;
-    cout << (flag == true) << endl;
+    unsigned a = 10;
+    int b = -100; // -100转为无符号数为（-100）% 4294967296 = 4294967196 
+    char c = 'A';
+    string d = "AB";
+    cout << a + b << endl;
+    cout << (long long)pow(2,32) << endl;
+    cout << d.size() << endl;
 
     return 0;
 } 
